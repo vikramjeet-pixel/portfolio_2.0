5a23c754c909096693f4970b545b7c0befd3d66a
vikramjeet singh <vikramjeetsinghs341@gmail.com>
2025-04-18 13:26:12 +0100

    added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://me.toinfinite.dev',
+            url: 'https://portfoliovikramjeetsingh.vercel.app/',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
