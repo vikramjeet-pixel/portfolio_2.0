09829d1a2fe5ef878594ac6d00379c57592ec817
vikramjeet singh <vikramjeetsinghs341@gmail.com>
2025-04-18 14:23:12 +0100

    added

--- app/_components/AboutMe.tsx
+++ app/_components/AboutMe.tsx
@@ -73,18 +73,16 @@ const AboutMe = () => {
                     <div className="md:col-span-7">
                         <div className="text-lg text-muted-foreground max-w-[450px]">
                             <p className="slide-up-and-fade">
-                                I&apos;m a frontend web developer dedicated to
+                                I&apos;m a full stack web developer dedicated to
                                 turning ideas into creative solutions. I
                                 specialize in creating seamless and intuitive
                                 user experiences.
                             </p>
                             <p className="mt-3 slide-up-and-fade">
-                                My approach focuses on creating scalable,
-                                high-performing solutions tailored to both user
-                                needs and business objectives. By prioritizing
-                                performance, accessibility, and responsiveness,
-                                I strive to deliver experiences that not only
-                                engage users but also drive tangible results.
+                            My approach emphasizes crafting scalable, high-performance solutions 
+                            designed to meet both user needs and business goals. 
+                            By focusing on performance, accessibility, and responsiveness, I
+                            aim to create experiences that captivate users while delivering measurable outcomes.
                             </p>
                         </div>
                     </div>
