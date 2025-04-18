913d284846250fa890e5e5d9cfd97ecc87363a3c
vikramjeet singh <vikramjeetsinghs341@gmail.com>
2025-04-18 01:49:32 +0100

    added

---
+++ README copy.md
@@ -0,0 +1,35 @@
+# 🚀 My Portfolio Website
+
+![image](https://github.com/user-attachments/assets/ade45c4b-b2cb-46cb-b8a6-31cad3bd0c5a)
+
+## 🚨 Forking this repo (please read!)
+
+Many people have contacted me asking me if they can use this code for their own website, and the answer to that question is usually **yes, with attribution**.
+
+I value keeping my site open source, but as you all know, _**plagiarism is bad**_. It's always disheartening whenever I find that someone has copied my site without giving me credit. I spent a non-trivial amount of effort building and designing this iteration of my website, and I am proud of it! All I ask of you all is to not claim this effort as your own.
+
+Please also note that I did not build this site with the intention of it being a starter theme, so if you have questions about implementation, please refer to the [Next.js docs](https://nextjs.org/docs).
+
+### TL;DR
+
+Yes, you can fork this repo. Please give me proper credit by linking back to [me.toinfinite.dev](https://www.me.toinfinite.dev/). Thanks!
+
+## 🛠 Installation & Set Up
+
+First, run the development server:
+
+```bash
+pnpm install
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## 🤝 Contributing
+If you'd like to contribute or suggest improvements, feel free to open an issue or submit a pull request. All contributions are welcome!
+
+## Credit
+_This readme is inspired from [Brittany Chiang's github](https://github.com/bchiang7/v4)_
+
+## 📄 License
+This project is open source and available under the [MIT License](https://github.com/Tajmirul/portfolio-2.0/blob/main/LICENSE).
---
+++ app/layout.tsx
@@ -0,0 +1,66 @@
+import type { Metadata } from 'next';
+import { Anton, Roboto_Flex } from 'next/font/google';
+import { ReactLenis } from 'lenis/react';
+
+import 'lenis/dist/lenis.css';
+import './globals.css';
+import Footer from '@/components/Footer';
+import ScrollProgressIndicator from '@/components/ScrollProgressIndicator';
+import ParticleBackground from '@/components/ParticleBackground';
+import Navbar from '@/components/Navbar';
+import CustomCursor from '@/components/CustomCursor';
+import Preloader from '../components/Preloader';
+import StickyEmail from './_components/StickyEmail';
+import { GoogleAnalytics } from '@next/third-parties/google';
+
+const antonFont = Anton({
+    weight: '400',
+    style: 'normal',
+    subsets: ['latin'],
+    variable: '--font-anton',
+});
+
+const robotoFlex = Roboto_Flex({
+    weight: ['100', '400', '500', '600', '700', '800'],
+    style: 'normal',
+    subsets: ['latin'],
+    variable: '--font-roboto-flex',
+});
+
+export const metadata: Metadata = {
+    title: 'Portfolio - Tajmirul Islam',
+    description: 'Personal portfolio of Tajmirul Islam',
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <GoogleAnalytics gaId="G-MHLY1LNGY5" />
+            <body
+                className={`${antonFont.variable} ${robotoFlex.variable} antialiased`}
+            >
+                <ReactLenis
+                    root
+                    options={{
+                        lerp: 0.1,
+                        duration: 1.4,
+                    }}
+                >
+                    <Navbar />
+                    <main>{children}</main>
+                    <Footer />
+
+                    <CustomCursor />
+                    <Preloader />
+                    <ScrollProgressIndicator />
+                    <ParticleBackground />
+                    <StickyEmail />
+                </ReactLenis>
+            </body>
+        </html>
+    );
+}
